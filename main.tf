caa2269cba8933014c2f70be8091b11eccb6c195
Nate Berkopec <nate.berkopec@gmail.com>
2020-04-21 14:21:19 +0900

    Revert "Del netlify"
    This reverts commit e710e4055d67186a23fa2c614e60f523e2ea032e.

---
+++ main.tf
@@ -0,0 +1,41 @@
+locals {
+  infra_name = "speedshop-dotcom"
+  region     = "us-east-1"
+}
+
+provider "aws" {
+  region = local.region
+}
+
+terraform {
+  backend "s3" {
+    bucket = "speedshop-terraform"
+    key    = "#{local.infra_name}-prod.tfstate"
+    region = "us-east-1"
+  }
+}
+
+data "aws_iam_policy_document" "bucket_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    sid       = "AllowPublicRead"
+    resources = ["arn:aws:s3:::${local.infra_name}/*"]
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+  }
+}
+
+resource "aws_s3_bucket" "website" {
+  bucket        = local.infra_name
+  region        = local.region
+  acl           = "public-read"
+  policy        = data.aws_iam_policy_document.bucket_policy.json
+  force_destroy = true
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+}
