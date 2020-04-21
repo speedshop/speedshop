2dc2c435abe3def71db74af82c8684eaf2aa2a7b
Nate Berkopec <nate.berkopec@gmail.com>
2020-04-21 13:57:52 +0900

    Revert "Nvm, we Netlify now"
    This reverts commit bda51a6854745713325bfa61360499281d949e06.

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
 locals {
-  infra_name = "speedshop-dotcom"
+  infra_name    = "speedshop-dotcom"
+  s3_origin_id  = "#{infra_name}-cloudfront"
+  region        = "us-east-1"
+}
+
+provider "aws" {
+  region = local.region
 }
 
 terraform {
@@ -10,27 +16,63 @@ terraform {
   }
 }
 
-provider "netlify" {}
-
-# Create a new deploy key for this specific website
-resource "netlify_deploy_key" "key" {}
-
-# Define your site
-resource "netlify_site" "main" {
-  name = local.infra_name
-
-  repo {
-    repo_branch   = "master"
-    command       = "jekyll build"
-    deploy_key_id = netlify_deploy_key.key.id
-    dir           = "_site"
-    provider      = "github"
-    repo_path     = "speedshop/speedshop"
+data "aws_iam_policy_document" "bucket_policy" {
+  statement {
+    actions = ["s3:GetObject"]
+    sid = "AllowPublicRead"
+    resources = ["arn:aws:s3:::${local.infra_name}/*"]
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
   }
-
-  custom_domain = "www.speedshop.co"
 }
 
-output "netlify_deploy_pubkey" {
-  value = netlify_deploy_key.key.public_key
+resource "aws_s3_bucket" "website" {
+  bucket = local.infra_name
+  region = local.region
+  acl    = "public-read"
+  policy = data.aws_iam_policy_document.bucket_policy.json
+  force_destroy = true
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.website.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
 }
