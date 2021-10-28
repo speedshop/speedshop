f54afce962a5976cec882aafbbb542609698acb7
Nate Berkopec <nate.berkopec@gmail.com>
2021-10-28 08:17:56 -0600

    format tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ terraform {
       version = "~> 3.0"
     }
     cloudflare = {
-      source = "cloudflare/cloudflare"
+      source  = "cloudflare/cloudflare"
       version = "~> 3.0"
     }
   }
