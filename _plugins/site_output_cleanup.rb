7ca5e0e2dbb9b3f2605fb9109dfc87fa17635c1d
Nate Berkopec <nate.berkopec@gmail.com>
2026-06-22 10:44:29 +0900

    Add dev-only type specimen page

--- _plugins/site_output_cleanup.rb
+++ _plugins/site_output_cleanup.rb
@@ -5,5 +5,8 @@ Jekyll::Hooks.register :site, :post_write do |site|
     FileUtils.rm_rf(File.join(site.dest, entry))
   end
 
-  FileUtils.rm_f(File.join(site.dest, "vertical_debug.html")) unless site.config["vertical_debug"]
+  unless site.config["vertical_debug"]
+    FileUtils.rm_f(File.join(site.dest, "type_specimen.html"))
+    FileUtils.rm_f(File.join(site.dest, "vertical_debug.html"))
+  end
 end
